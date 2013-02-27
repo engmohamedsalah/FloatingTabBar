aec83a779ff3f23cbd4d314067c7122f3617f09c
Jonathan Nesbitt <jon.nesbitt@gmail.com>
2013-02-27 10:30:03 -0500

    Converted tabs to four spaces.

--- FloatingTabBarController/FloatingTabBarController.cs
+++ FloatingTabBarController/FloatingTabBarController.cs
@@ -5,111 +5,111 @@ using MonoTouch.UIKit;
 
 namespace FloatingTabBar
 {
-	public class FloatingTabBarController : UIViewController
-	{
-		List<string> tabTitles;
-		List<UIButton> tabButtons;
-		List<UIViewController> viewControllers;
-		UIView tabView;
-		float tabWidth;
-		
-		public List<string> TabTitles {
-			get {
-				return tabTitles;
-			}
-			set {
-				tabTitles = value;
+    public class FloatingTabBarController : UIViewController
+    {
+        List<string> tabTitles;
+        List<UIButton> tabButtons;
+        List<UIViewController> viewControllers;
+        UIView tabView;
+        float tabWidth;
 
-				if ((tabTitles != null) && (tabTitles.Count > 0)) {
-					
-					// Create tab buttons.
-					tabButtons.Clear ();
-					tabWidth = 0;
-					int tabIndex = 0;
-					foreach (var title in tabTitles) {
-						var button = new UIButton (UIButtonType.Custom);
-						button.ContentEdgeInsets = new UIEdgeInsets (5, 10, 5, 10);
-						button.BackgroundColor = UIColor.White;
-						button.SetTitleColor (UIColor.FromRGB (71, 71, 71), UIControlState.Normal);
-						button.SetTitle (title, UIControlState.Normal);
-						button.SizeToFit ();
-						button.Tag = tabIndex++;
-						button.TouchUpInside += (sender, e) => {
-							SelectTab (button.Tag);
-						};
-						tabButtons.Add (button);
-						tabView.AddSubview (button);
-						tabWidth += button.Frame.Width;
-					}
-					
-					// Update tab button locations.
-					float currentWidth = 0;
-					foreach (var button in tabButtons) {
-						button.Frame = new RectangleF (currentWidth, 0, button.Frame.Width, button.Frame.Height);
-						currentWidth += button.Frame.Width;
-					}
-					
-					tabView.Frame = new RectangleF ((View.Center.X - (tabWidth / 2)), 20, tabWidth, tabButtons[0].Frame.Height);
-					
-					SelectTab (0);
-				}
-			}
-		}
-		
-		public List<UIViewController> ViewControllers {
-			get {
-				return viewControllers;
-			}
-			set {
-				viewControllers = value;
-				SelectTab (0);
-			}
-		}
-		
-		public int SelectedIndex {
-			get;
-			private set;
-		}
-		
-		public UIViewController SelectedViewController {
-			get;
-			private set;
-		}
-		
-		UIView SelectedView {
-			get {
-				if (SelectedViewController != null)
-					return SelectedViewController.View;
-				else
-					return null;
-			}
-		}
-		
-		void SelectTab (int index)
-		{
-			if ((viewControllers == null) || (viewControllers.Count <= index) || (index < 0) || (viewControllers [index] == null))
-				return;
-			
-			SelectedIndex = index;
-			SelectedViewController = viewControllers [index];
-			View.InsertSubviewBelow (SelectedView, tabView);
+        public List<string> TabTitles {
+            get {
+                return tabTitles;
+            }
+            set {
+                tabTitles = value;
 
-			// Update selected tab.
-			foreach (var button in tabButtons) {
-				button.BackgroundColor = UIColor.White;
-				button.SetTitleColor (UIColor.FromRGB (71, 71, 71), UIControlState.Normal);
-			}
-			tabButtons [SelectedIndex].BackgroundColor = UIColor.FromRGB (71, 71, 71);
-			tabButtons [SelectedIndex].SetTitleColor (UIColor.White, UIControlState.Normal);
-		}
-		
-		public FloatingTabBarController ()
-		{
-			View.Frame = UIScreen.MainScreen.Bounds;
-			tabView = new UIView (new RectangleF (View.Center.X, 20, 0, 0));
-			View.AddSubview (tabView);
-			tabButtons = new List<UIButton> ();
-		}
-	}
+                if ((tabTitles != null) && (tabTitles.Count > 0)) {
+
+                    // Create tab buttons.
+                    tabButtons.Clear ();
+                    tabWidth = 0;
+                    int tabIndex = 0;
+                    foreach (var title in tabTitles) {
+                        var button = new UIButton (UIButtonType.Custom);
+                        button.ContentEdgeInsets = new UIEdgeInsets (5, 10, 5, 10);
+                        button.BackgroundColor = UIColor.White;
+                        button.SetTitleColor (UIColor.FromRGB (71, 71, 71), UIControlState.Normal);
+                        button.SetTitle (title, UIControlState.Normal);
+                        button.SizeToFit ();
+                        button.Tag = tabIndex++;
+                        button.TouchUpInside += (sender, e) => {
+                            SelectTab (button.Tag);
+                        };
+                        tabButtons.Add (button);
+                        tabView.AddSubview (button);
+                        tabWidth += button.Frame.Width;
+                    }
+
+                    // Update tab button locations.
+                    float currentWidth = 0;
+                    foreach (var button in tabButtons) {
+                        button.Frame = new RectangleF (currentWidth, 0, button.Frame.Width, button.Frame.Height);
+                        currentWidth += button.Frame.Width;
+                    }
+
+                    tabView.Frame = new RectangleF ((View.Center.X - (tabWidth / 2)), 20, tabWidth, tabButtons [0].Frame.Height);
+
+                    SelectTab (0);
+                }
+            }
+        }
+
+        public List<UIViewController> ViewControllers {
+            get {
+                return viewControllers;
+            }
+            set {
+                viewControllers = value;
+                SelectTab (0);
+            }
+        }
+
+        public int SelectedIndex {
+            get;
+            private set;
+        }
+
+        public UIViewController SelectedViewController {
+            get;
+            private set;
+        }
+
+        UIView SelectedView {
+            get {
+                if (SelectedViewController != null)
+                    return SelectedViewController.View;
+                else
+                    return null;
+            }
+        }
+
+        void SelectTab (int index)
+        {
+            if ((viewControllers == null) || (viewControllers.Count <= index) || (index < 0) || (viewControllers [index] == null))
+                return;
+
+            SelectedIndex = index;
+            SelectedViewController = viewControllers [index];
+            View.InsertSubviewBelow (SelectedView, tabView);
+
+            // Update selected tab.
+            foreach (var button in tabButtons) {
+                button.BackgroundColor = UIColor.White;
+                button.SetTitleColor (UIColor.FromRGB (71, 71, 71), UIControlState.Normal);
+            }
+            tabButtons [SelectedIndex].BackgroundColor = UIColor.FromRGB (71, 71, 71);
+            tabButtons [SelectedIndex].SetTitleColor (UIColor.White, UIControlState.Normal);
+        }
+
+        public FloatingTabBarController ()
+        {
+            View.Frame = UIScreen.MainScreen.Bounds;
+            tabView = new UIView (new RectangleF (View.Center.X, 20, 0, 0));
+            View.AddSubview (tabView);
+            tabButtons = new List<UIButton> ();
+        }
+    }
 }
 
--- FloatingTabBarControllerDemo/AppDelegate.cs
+++ FloatingTabBarControllerDemo/AppDelegate.cs
@@ -10,77 +10,77 @@ using FloatingTabBar;
 
 namespace FloatingTabBarControllerDemo
 {
-	// The UIApplicationDelegate for the application. This class is responsible for launching the 
-	// User Interface of the application, as well as listening (and optionally responding) to 
-	// application events from iOS.
-	[Register ("AppDelegate")]
-	public partial class AppDelegate : UIApplicationDelegate
-	{
-		// class-level declarations
-		UIWindow window;
-		FloatingTabBarController ftbc;
-		DialogViewController dvc1, dvc2, dvc3;
-		UIViewController mapViewController;
-		MKMapView mapView;
+    // The UIApplicationDelegate for the application. This class is responsible for launching the 
+    // User Interface of the application, as well as listening (and optionally responding) to 
+    // application events from iOS.
+    [Register ("AppDelegate")]
+    public partial class AppDelegate : UIApplicationDelegate
+    {
+        // class-level declarations
+        UIWindow window;
+        FloatingTabBarController ftbc;
+        DialogViewController dvc1, dvc2, dvc3;
+        UIViewController mapViewController;
+        MKMapView mapView;
 
-		//
-		// This method is invoked when the application has loaded and is ready to run. In this 
-		// method you should instantiate the window, load the UI into it and then make the window
-		// visible.
-		//
-		// You have 17 seconds to return from this method, or iOS will terminate your application.
-		//
-		public override bool FinishedLaunching (UIApplication app, NSDictionary options)
-		{
-			// create a new window instance based on the screen size
-			window = new UIWindow (UIScreen.MainScreen.Bounds);
+        //
+        // This method is invoked when the application has loaded and is ready to run. In this 
+        // method you should instantiate the window, load the UI into it and then make the window
+        // visible.
+        //
+        // You have 17 seconds to return from this method, or iOS will terminate your application.
+        //
+        public override bool FinishedLaunching (UIApplication app, NSDictionary options)
+        {
+            // create a new window instance based on the screen size
+            window = new UIWindow (UIScreen.MainScreen.Bounds);
 
-			dvc1 = new DialogViewController (null) {
-				Root = new RootElement ("Tab 1 DVC") {
-					new Section (), new Section (), new Section (),
-					new Section () {
-						new StringElement ("House of Cards"),
-						new StringElement ("Zombieland")
-					}
-				}
-			};
+            dvc1 = new DialogViewController (null) {
+                Root = new RootElement ("Tab 1 DVC") {
+                    new Section (), new Section (), new Section (),
+                    new Section () {
+                        new StringElement ("House of Cards"),
+                        new StringElement ("Zombieland")
+                    }
+                }
+            };
 
-			dvc2 = new DialogViewController (null) {
-				Root = new RootElement ("Tab 2 DVC") {
-					new Section (), new Section (), new Section (),
-					new Section () {
-						new StringElement ("Adventure Time"),
-						new StringElement ("Bob's Burgers")
-					}
-				}
-			};
+            dvc2 = new DialogViewController (null) {
+                Root = new RootElement ("Tab 2 DVC") {
+                    new Section (), new Section (), new Section (),
+                    new Section () {
+                        new StringElement ("Adventure Time"),
+                        new StringElement ("Bob's Burgers")
+                    }
+                }
+            };
 
-			dvc3 = new DialogViewController (null) {
-				Root = new RootElement ("Tab 3 DVC") {
-					new Section (), new Section (), new Section (),
-					new Section () {
-						new StringElement ("Braid"),
-						new StringElement ("Super Meat Boy")
-					}
-				}
-			};
+            dvc3 = new DialogViewController (null) {
+                Root = new RootElement ("Tab 3 DVC") {
+                    new Section (), new Section (), new Section (),
+                    new Section () {
+                        new StringElement ("Braid"),
+                        new StringElement ("Super Meat Boy")
+                    }
+                }
+            };
 
-			mapViewController = new UIViewController ();
-			mapView = new MKMapView (new RectangleF (0, -20, window.Bounds.Width, window.Bounds.Height));
-			mapViewController.View.AddSubview (mapView);
+            mapViewController = new UIViewController ();
+            mapView = new MKMapView (new RectangleF (0, -20, window.Bounds.Width, window.Bounds.Height));
+            mapViewController.View.AddSubview (mapView);
 
-			ftbc = new FloatingTabBarController () {
-				TabTitles = new List<string> () { "Tab 1", "Tab 2", "Tab 3", "Map" },
-				ViewControllers = new List<UIViewController> () { dvc1, dvc2, dvc3, mapViewController }
-			};
+            ftbc = new FloatingTabBarController () {
+                TabTitles = new List<string> () { "Tab 1", "Tab 2", "Tab 3", "Map" },
+                ViewControllers = new List<UIViewController> () { dvc1, dvc2, dvc3, mapViewController }
+            };
 
-			window.RootViewController = ftbc;
-			
-			// make the window visible
-			window.MakeKeyAndVisible ();
-			
-			return true;
-		}
-	}
+            window.RootViewController = ftbc;
+            
+            // make the window visible
+            window.MakeKeyAndVisible ();
+            
+            return true;
+        }
+    }
 }
 
