1353c3a01cad6a083a7ba9808ebf3cc2197aa55d
Jonathan Nesbitt <jon.nesbitt@gmail.com>
2013-02-27 10:54:27 -0500

    Added blank launch images, and cleaned-up the demo.

--- FloatingTabBarControllerDemo/AppDelegate.cs
+++ FloatingTabBarControllerDemo/AppDelegate.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
+using MonoTouch.CoreLocation;
 using MonoTouch.Dialog;
 using MonoTouch.Foundation;
 using MonoTouch.MapKit;
@@ -10,73 +11,55 @@ using FloatingTabBar;
 
 namespace FloatingTabBarControllerDemo
 {
-    // The UIApplicationDelegate for the application. This class is responsible for launching the 
-    // User Interface of the application, as well as listening (and optionally responding) to 
-    // application events from iOS.
     [Register ("AppDelegate")]
     public partial class AppDelegate : UIApplicationDelegate
     {
-        // class-level declarations
         UIWindow window;
         FloatingTabBarController ftbc;
-        DialogViewController dvc1, dvc2, dvc3;
+        DialogViewController dvc1, dvc2;
         UIViewController mapViewController;
-        MKMapView mapView;
 
-        //
-        // This method is invoked when the application has loaded and is ready to run. In this 
-        // method you should instantiate the window, load the UI into it and then make the window
-        // visible.
-        //
-        // You have 17 seconds to return from this method, or iOS will terminate your application.
-        //
         public override bool FinishedLaunching (UIApplication app, NSDictionary options)
         {
-            // create a new window instance based on the screen size
             window = new UIWindow (UIScreen.MainScreen.Bounds);
 
+            // Create the 'Shows' tab.
             dvc1 = new DialogViewController (null) {
-                Root = new RootElement ("Tab 1 DVC") {
+                Root = new RootElement ("") {
                     new Section (), new Section (), new Section (),
                     new Section () {
                         new StringElement ("House of Cards"),
-                        new StringElement ("Zombieland")
+                        new StringElement ("Game of Thrones"),
+                        new StringElement ("Person of Interest")
                     }
                 }
             };
 
+            // Create the 'Games' tab.
             dvc2 = new DialogViewController (null) {
-                Root = new RootElement ("Tab 2 DVC") {
-                    new Section (), new Section (), new Section (),
-                    new Section () {
-                        new StringElement ("Adventure Time"),
-                        new StringElement ("Bob's Burgers")
-                    }
-                }
-            };
-
-            dvc3 = new DialogViewController (null) {
-                Root = new RootElement ("Tab 3 DVC") {
+                Root = new RootElement ("") {
                     new Section (), new Section (), new Section (),
                     new Section () {
                         new StringElement ("Braid"),
-                        new StringElement ("Super Meat Boy")
+                        new StringElement ("Super Meat Boy"),
+                        new StringElement ("Fez")
                     }
                 }
             };
 
+            // Create the 'Atlanta' tab.
+            var mapView = new MKMapView (new RectangleF (0, -20, window.Bounds.Width, window.Bounds.Height));
+            var atlanta = new MKCoordinateRegion (new CLLocationCoordinate2D (33.748893, -84.388046), new MKCoordinateSpan (0.35, 0.35));
+            mapView.SetRegion (atlanta, false);
             mapViewController = new UIViewController ();
-            mapView = new MKMapView (new RectangleF (0, -20, window.Bounds.Width, window.Bounds.Height));
             mapViewController.View.AddSubview (mapView);
 
+            // Create and display the floating tab bar controller.
             ftbc = new FloatingTabBarController () {
-                TabTitles = new List<string> () { "Tab 1", "Tab 2", "Tab 3", "Map" },
-                ViewControllers = new List<UIViewController> () { dvc1, dvc2, dvc3, mapViewController }
+                TabTitles = new List<string> () { "Shows", "Games", "Atlanta" },
+                ViewControllers = new List<UIViewController> () { dvc1, dvc2, mapViewController }
             };
-
             window.RootViewController = ftbc;
-            
-            // make the window visible
             window.MakeKeyAndVisible ();
             
             return true;
